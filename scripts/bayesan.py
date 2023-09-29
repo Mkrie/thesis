910e234074ab0215d0362390abd1d6facf2a7836
Danil Shamsutdinov <a.danil.dark@gmail.com>
2023-09-29 18:09:46 +0300

    additions

--- scripts/bayesan.py
+++ scripts/bayesan.py
@@ -6,18 +6,4 @@ from pathlib import Path
 from dataclasses import dataclass
 
 
-@dataclass
-class Bayesan:
-    """Class for optimal statistical estimation"""
-
-    profile: str
-    profile_type: str
-    dataset_name: str
-
-    def get_data(self):
-        """Get matrix M, direct measurements xi"""
-        ...
-
-
-if __name__ == "__main__":
-    ...
+print(5 * np.eye(5))
